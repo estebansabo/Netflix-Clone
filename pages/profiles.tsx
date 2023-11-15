7022cd41dd98316b104315b81b0f90eaada23cd7
Esteban Sabo G <estebanzsabo@gmail.com>
2023-11-15 12:20:19 -0400

    Update profiles.tsx

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -1,6 +1,7 @@
 import useCurrentUser from '@/hooks/useCurrentUser';
 import { NextPageContext } from 'next';
 import { getSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 export async function getServerSideProps(context: NextPageContext) {
     const session = await getSession(context);
@@ -19,6 +20,7 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 const Profiles = () => {
+    const router = useRouter();
     const {data: user } = useCurrentUser();
 
     return (
@@ -26,7 +28,7 @@ const Profiles = () => {
             <div className="flex flex-col">
                 <h1 className="text-3xl md:text-6xl text-white text-center">Who is Watching?</h1>
                 <div className="flex items-center justify-center gap-8 mt-10">
-                    <div onClick={() => {}}>
+                    <div onClick={() => router.push('/')}>
                         
                         <div className="group flex-row w-44 mx-auto:">
                             <div className="w-44 h-44 rounded-md flex items-center justify-center border-2 border-transparent group-hover:cursor-pointer group-hover:border-white overflow-hidden">
