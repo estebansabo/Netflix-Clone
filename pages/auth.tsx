c72bf5552756060fb6fe6ea38e2b50ba1f39a5d7
Esteban Sabo G <estebanzsabo@gmail.com>
2023-11-15 11:43:50 -0400

    first hook

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,6 +1,6 @@
 import { useCallback, useState } from "react";
 import axios from "axios";
-import Input from "@/components/input";
+import Input from "@/components/Input";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/router";
 import { FcGoogle } from "react-icons/fc";
