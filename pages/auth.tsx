0b24fb9bcdcc6da72c186aff44031a3a1eaba20f
Esteban Sabo G <estebanzsabo@gmail.com>
2023-11-27 14:39:32 -0400

    Update auth.tsx

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -5,7 +5,7 @@ import { getSession, signIn } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import { FcGoogle } from 'react-icons/fc';
 import { FaGithub } from 'react-icons/fa';
-// @ts-ignore 
+
 import Input from '@/components/Input';
 
 export async function getServerSideProps(context: NextPageContext) {
